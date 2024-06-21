973c869902d94299e0d584d5abb9a90e2387eea2
chuyongli <t.palmo@hotmail.com>
2024-06-21 00:11:47 -0400

    Suggested adding code plan.

--- Class Assignments/Assignment2_BilalMohideen.R
+++ Class Assignments/Assignment2_BilalMohideen.R
@@ -1,6 +1,8 @@
 #Assignment 2 - Narcissistic Numbers
 #Bilal Mohideen - BTC1855H
 
+# (TP) It wasn't a requirement for this assignment but you could potentially add a step-by-step plan here.
+
 #Prompts the user to enter a three digit positive number
 answer <- readline(prompt = "Please enter a three digit positive number: ")
 
